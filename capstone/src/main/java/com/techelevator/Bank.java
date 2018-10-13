867dcdc8c48599f8adff6c6a7b30268897f45cf3
Abigale Ruminas <ahardin7@kent.edu>
2018-10-12 20:44:18 -0400

    fridaynight

--- capstone/src/main/java/com/techelevator/Bank.java
+++ capstone/src/main/java/com/techelevator/Bank.java
@@ -4,41 +4,40 @@ import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Bank {
-	
-	private BigDecimal balance = new BigDecimal ("0.00"); 
-		
+
+	private BigDecimal balance = new BigDecimal("0.00");
+
 	public void getMoney() {
-	
+
 		Scanner customerInput = new Scanner(System.in);
 
-		while(true) {
+		while (true) {
 			System.out.println("Please enter $: 1, 2, 5 or 10)");
-	
+
 			BigDecimal cashIn = customerInput.nextBigDecimal();
 			customerInput.nextLine();
-		
-			System.out.println("cashIn = "+ cashIn);
-	
-			if (cashIn.equals(new BigDecimal(1))  
-			 || cashIn.equals(new BigDecimal(2))
-			 || cashIn.equals(new BigDecimal(5))
-			 || cashIn.equals(new BigDecimal(10))) {
+
+			System.out.println("cashIn = " + cashIn);
+
+			if (cashIn.equals(new BigDecimal(1)) || cashIn.equals(new BigDecimal(2)) || cashIn.equals(new BigDecimal(5))
+					|| cashIn.equals(new BigDecimal(10))) {
 				balance = balance.add(cashIn);
 				System.out.println("current balance: $" + balance.toString());
-			}
-			else {
+			} else {
 				System.out.println("Invalid Input please enter 1,2,5 or 10");
 			}
 			System.out.println("Do you want to add more money? If so y for yes n for no");
 			String customerWantsMore = customerInput.nextLine();
-			
-			if(customerWantsMore.toLowerCase().equals("n")) {
+
+			if (customerWantsMore.toLowerCase().equals("n")) {
 				break;
 			}
-			
-	}				
+
+		}
+
+	}
+	public BigDecimal withdraw(BigDecimal amountToWithdraw) {
+		balance = balance.subtract(amountToWithdraw);
+			return balance;
+	}
 }
-}
-
-
-
--- capstone/src/main/java/com/techelevator/VendingMachine.java
+++ capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -4,24 +4,33 @@ import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class VendingMachine {
-	
+
 	private Inventory inventory = new Inventory();
-	
+
 	private Bank bank = new Bank();
-	
+
 	public void getSelection() {
 		@SuppressWarnings("resource")
 		Scanner customerSelection = new Scanner(System.in);
 		System.out.println("Please select the slot id for the product you want: ");
 		String selection = customerSelection.nextLine();
-		
-		Product customerProduct = inventory.findProduct(selection.toUpperCase());
-		int newQuantity = customerProduct.getQuantity()-1;
-			customerProduct.setQuantity(newQuantity);
-			System.out.println(customerProduct.getName()+ " " + customerProduct.getPrice());
-		}
-	
 
+		Product customerProduct = inventory.findProduct(selection.toUpperCase());
+		int newQuantity = customerProduct.getQuantity() - 1;
+		customerProduct.setQuantity(newQuantity);
+		System.out.println(customerProduct.getName() + " " + customerProduct.getPrice());
+		
+		bank.withdraw(customerProduct.getPrice());
+	}
+
+	public void createChange () {
+		while(true) {
+			
+		}
+			
+		Bank currentBalance.
+		
+	}
 
 	public Bank getBank() {
 		return bank;
@@ -30,8 +39,8 @@ public class VendingMachine {
 	public void boot() throws Exception {
 		inventory.stock();
 	}
-	
-    public Inventory getInventory() {
-    	return inventory;
-    }
+
+	public Inventory getInventory() {
+		return inventory;
+	}
 }
